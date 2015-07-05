48d611d5f075a3b682d9eec345452cd19785a428
Patrik Wenger <paddor@gmail.com>
2015-07-05 14:57:01 +0200

    provide apt-get install with -y so it doesn't ask

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ utopic main universe"
 sudo apt-get update -qq
-sudo apt-get install check cmake
+sudo apt-get install check cmake -y
 scripts/install-libsodium.sh
