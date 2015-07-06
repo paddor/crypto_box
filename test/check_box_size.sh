50139a2dfd8208e67aa8ee930a668a2b4be56453
Patrik Wenger <paddor@gmail.com>
2015-07-06 04:04:08 +0200

    oh non-BSD version of mktemp :S

--- test/check_box_size.sh
+++ test/check_box_size.sh
@@ -2,6 +2,6 @@
 set -e
 lock_box=../lock_box
 TXT_FILE=`dirname $0`/lorem.txt
-BOX_FILE=`mktemp -t lorem.box`
+BOX_FILE=`mktemp -t lorem.box.XXX`
 $lock_box < $TXT_FILE > $BOX_FILE
 (( `stat -f %z $TXT_FILE` == `stat -f %z $BOX_FILE` - 41))
--- test/round_trip.sh
+++ test/round_trip.sh
@@ -2,10 +2,10 @@
 set -e
 lock_box=../lock_box
 open_box=../open_box
-KEY_FILE=`mktemp -ut round_trip.key` # -u: don't wanna create the keyfile yet
+KEY_FILE=`mktemp -ut round_trip.key.XXX` # -u: don't wanna create the keyfile yet
 TXT_FILE=`dirname $0`/lorem.txt
-BOX_FILE=`mktemp -t lorem.box`
-NEW_TXT_FILE=`mktemp -t lorem.txt`
+BOX_FILE=`mktemp -t lorem.box.XXX`
+NEW_TXT_FILE=`mktemp -t lorem.txt.XXX`
 $lock_box -k $KEY_FILE < $TXT_FILE > $BOX_FILE
 $open_box -k $KEY_FILE < $BOX_FILE > $NEW_TXT_FILE
 diff $TXT_FILE $NEW_TXT_FILE
