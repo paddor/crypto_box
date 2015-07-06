d0570279f5090efdd93aa0ed45871af1b5f2109a
Patrik Wenger <paddor@gmail.com>
2015-07-06 03:45:21 +0200

    create temp files for test in a better place

--- test/round_trip.sh
+++ test/round_trip.sh
@@ -2,10 +2,10 @@
 set -e
 lock_box=../lock_box
 open_box=../open_box
-KEY_FILE=round_trip.key
+KEY_FILE=`mktemp -ut round_trip.key` # -u: don't wanna create the keyfile yet
 TXT_FILE=`dirname $0`/lorem.txt
-BOX_FILE=lorem.box
-NEW_TXT_FILE=lorem.txt
+BOX_FILE=`mktemp -t lorem.box`
+NEW_TXT_FILE=`mktemp -t lorem.txt`
 $lock_box -k $KEY_FILE < $TXT_FILE > $BOX_FILE
 $open_box -k $KEY_FILE < $BOX_FILE > $NEW_TXT_FILE
 diff $TXT_FILE $NEW_TXT_FILE
