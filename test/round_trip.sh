dc3df6d9f5feff2dbd3861a19aa326c0a066af3e
Patrik Wenger <paddor@gmail.com>
2015-07-08 22:13:50 +0200

    pass path to input file as argument

--- test/round_trip.sh
+++ test/round_trip.sh
@@ -3,7 +3,7 @@ set -e
 lock_box=../lock_box
 open_box=../open_box
 KEY_FILE=`mktemp -ut round_trip.key.XXX` # -u: don't wanna create the keyfile yet
-TXT_FILE=`dirname $0`/lorem.txt
+TXT_FILE="$1"
 BOX_FILE=`mktemp -t lorem.box.XXX`
 NEW_TXT_FILE=`mktemp -t lorem.txt.XXX`
 $lock_box -k $KEY_FILE < $TXT_FILE > $BOX_FILE
