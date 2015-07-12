e4245fa8cee78358ed984f058f2b2089066d0b50
Patrik Wenger <paddor@gmail.com>
2015-07-12 20:18:19 +0200

    improve readability

--- test/round_trip_big.sh
+++ test/round_trip_big.sh
@@ -2,8 +2,9 @@
 set -e
 TXT_FILE="$1"
 BIG_FILE="big.txt"
+CHUNK_SIZE=262144
 touch $BIG_FILE
-while [ `wc -c < $BIG_FILE` -le 262144 ]
+while [ `wc -c < $BIG_FILE` -le $CHUNK_SIZE ]
 do
 	cat $TXT_FILE >> $BIG_FILE
 done
