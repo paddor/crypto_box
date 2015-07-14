593e91d75dd2b43490976f228538ee501291980d
Patrik Wenger <paddor@gmail.com>
2015-07-14 04:55:23 +0200

    simplify test shell script

--- test/round_trip_big.sh
+++ test/round_trip_big.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -x
 set -e
 TXT_FILE="$1"
-BIG_FILE="big.txt"
+BIG_FILE=`mktemp -t round_trip.pt.XXXXXX`
 CHUNK_SIZE=65535
-touch $BIG_FILE
 while [ `wc -c < $BIG_FILE` -le $CHUNK_SIZE ]
 do
 	cat $TXT_FILE >> $BIG_FILE
